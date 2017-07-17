1172b16ea6e273dea352ad01d7bcba19bd854f2b
achimbaggins <achim_baggins@yahoo.com>
2017-07-17 22:31:01 +0700

    Auth added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const fs = require('fs');
+const session = require('express-session');
 var app = express()
 const db = require('./models');
 
@@ -22,9 +25,44 @@ const teachers = require('./routers/teacher');
 const subjects = require('./routers/subject');
 const students = require('./routers/student');
 
+var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {flags: 'a'});
+app.use(morgan('combined',{stream: accessLogStream}));
+
+app.use(session({
+  secret: '56!@#$!#2346234626!@#$!!@#$',
+  resave: false,
+  saveUnitialized: true,
+  cookies: {}
+}))
+
 app.use('/', index)
-app.use('/teachers', teachers)
-app.use('/subjects', subjects)
+
+
+app.use((req, res, next) => {
+  if(req.session.authority > 0){
+    next()
+  } else {
+    res.sendStatus(403);
+  }
+})
 app.use('/students', students)
+
+app.use((req, res, next) => {
+  if(req.session.authority > 1){
+    next()
+  } else {
+    res.sendStatus(403);
+  }
+})
+app.use('/subjects', subjects)
+
+app.use((req, res, next) => {
+  if(req.session.authority === 3){
+    next()
+  } else {
+    res.sendStatus(403);
+  }
+})
+app.use('/teachers', teachers)
 
 app.listen(3000)
